fff061af88952adf783e1f6255d4d60648d8013c
Yerlan Kussainov <lagox.mail@gmail.com>
2012-07-28 10:30:48 +0600

    basic authentication

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,11 @@ end
 
 module OnlineBlog
   class Application < Rails::Application
+    
+    # disabled field_with_errors
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| 
+      "#{html_tag}".html_safe  }
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,13 @@
+# -*- encoding : utf-8 -*-
+require 'spec_helper'
+
+describe SessionsController do
+
+  describe "GET 'new'" do
+    it "returns http success" do
+      get 'new'
+      response.should be_success
+    end
+  end
+
+end
